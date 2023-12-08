4c97fe3977a027917b2603e459d925f27fe6fb96
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-12-08 14:36:44 +0200

    add Menu to the Header component

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -8,13 +8,22 @@ import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.svg";
 import { Link, useLocation } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
-const Links = [
+const tabLinks = [
   { label: "Home", href: "/" },
   { label: "Services", href: "/services" },
   { label: "The revolution", href: "/revolution" },
   { label: "About us", href: "/about" },
   { label: "Contact us", href: "/contact" },
+];
+
+const menuLinks = [
+  { label: "Services", href: "/services" },
+  { label: "Custom Software Development", href: "/customsoftware" },
+  { label: "Mobile App Development", href: "/mobileapps" },
+  { label: "Website Development", href: "/websites" },
 ];
 
 function ElevationScroll(props) {
@@ -42,7 +51,7 @@ const useStyles = makeStyles((theme) => ({
     padding: 0,
     "&:hover": {
       backgroundColor: "transparent",
-    },  
+    },
   },
   tabContainer: {
     marginLeft: "auto",
@@ -59,27 +68,64 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "25px",
     height: "45px",
   },
+  menu: {
+    backgroundColor: theme.palette.common.blue,
+    color: "white",
+    borderRadius: "0px",
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    "&:hover": {
+      opacity: 1,
+    },
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
-  const [value, setValue] = useState(0);
-
   const { pathname } = useLocation();
 
-  const handleChange = (event, value) => {
-    setValue(value);
+  const indexTab = tabLinks.findIndex((link) => link.href === pathname);
+  const selectOption = menuLinks.findIndex(({ href }) => href === pathname);
+
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleChange = (event, index) => {
+    setValue(index);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  const handleClick = () => (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleMenuItemClick = (index) => () => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(index);
+    handleClose();
   };
 
   useEffect(() => {
-    const index = Links.findIndex((link) => link.href === pathname);
-
-    if (index === -1 && value !== -1) {
+    if (indexTab === -1 && value !== -1) {
       setValue(-1);
-    } else if (index !== -1 && value !== index) {
-      setValue(index);
+    } else if (indexTab !== -1 && value !== indexTab) {
+      setValue(indexTab);
     }
-  }, [pathname, value, Links]);
+
+    if (selectOption !== -1) {
+      setValue(1);
+    }
+  }, [pathname, value, tabLinks]);
   return (
     <>
       <ElevationScroll>
@@ -99,14 +145,28 @@ const Header = () => {
               className={classes.tabContainer}
               indicatorColor="primary"
             >
-              {Links.map(({ label, href }) => (
-                <Tab
-                  component={Link}
-                  to={href}
-                  className={classes.tab}
-                  label={label}
-                />
-              ))}
+              {tabLinks.map(({ label, href }) =>
+                href === "/services" ? (
+                  <Tab
+                    key={href}
+                    aria-owns={anchorEl ? "simple-menu" : undefined}
+                    aria-haspopup={anchorEl ? "true" : undefined}
+                    onMouseOver={handleClick(event)}
+                    className={classes.tab}
+                    component={Link}
+                    to={href}
+                    label={label}
+                  />
+                ) : (
+                  <Tab
+                    key={href}
+                    className={classes.tab}
+                    component={Link}
+                    to={href}
+                    label={label}
+                  />
+                )
+              )}
             </Tabs>
             <Button
               component={Link}
@@ -117,6 +177,28 @@ const Header = () => {
             >
               Free Estimate
             </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
+              classes={{ paper: classes.menu }}
+              elevation={0}
+            >
+              {menuLinks.map(({ label, href }, index) => (
+                <MenuItem
+                  key={href}
+                  component={Link}
+                  to={href}
+                  onClick={handleMenuItemClick(index)}
+                  selected={index === selectedIndex && value === 1}
+                  classes={{ root: classes.menuItem }}
+                >
+                  {label}
+                </MenuItem>
+              ))}
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
